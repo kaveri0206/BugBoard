bcc2b478dc1cf9f12b9d882be1fa1db7b6c1105a
Kaveri Kadu <kaverirkadu02@gmail.com>
2026-09-21 12:10:25 +0530

    Fix CORS for Vercel frontend

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ app.use(
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'http://localhost:3000',
+      'https://bug-board-nu.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
